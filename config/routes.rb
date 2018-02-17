86f5f7bc788db4e5baf3e3f307573daf7aa05a12
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2018-02-17 23:52:33 +0900

    [add] song_histories controller 追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root  'welcomes#index'
+  resources :song_histories, only: :index
 
   get '/auth/spotify/callback', to: 'count_songs#index'
 end
