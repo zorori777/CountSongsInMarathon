0d97327eade5d74d0bb38a3ad5ee2057c62552d8
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2018-02-24 21:11:15 +0900

    [add] preview_url カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180223131440) do
+ActiveRecord::Schema.define(version: 20180224120615) do
 
   create_table "locations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "address", null: false
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20180223131440) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "location_id"
+    t.string "preview_url"
     t.index ["location_id"], name: "index_songs_on_location_id"
   end
 
